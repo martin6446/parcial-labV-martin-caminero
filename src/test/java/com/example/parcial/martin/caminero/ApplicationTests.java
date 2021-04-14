431ba3ec5a24934af15f5d91b6543624b32fdcf8
Martin Caminero <martincaminero64@gmail.com>
2021-04-14 16:15:33 -0300

    first commit

---
+++ src/test/java/com/example/parcial/martin/caminero/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parcial.martin.caminero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
